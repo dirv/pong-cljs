8f5378eb2733d31b49ccc3bc517b502f37b660e2
Daniel Irvine <daniel@danielirvine.com>
2017-05-27 09:53:09 +0100

    Starting point for Socrates Italy 2017

--- src/cljs/pong_cljs/core.cljs
+++ src/cljs/pong_cljs/core.cljs
@@ -12,10 +12,10 @@
 (def paddle-right-down \J)
 (def paddle-move-distance 10)
 
-(def state (atom {:position [200 200]
+(def state (atom {:position [320 240]
                   :velocity [1 1]
                   :paddle-left [30 30]
-                  :paddle-right [610 450]}))
+                  :paddle-right [610 (- 450 paddle-height)]}))
 
 (defn- draw-circle [ctx x y r]
   (.beginPath ctx)
@@ -37,51 +37,16 @@
   (render-paddle ctx :paddle-left)
   (render-paddle ctx :paddle-right))
 
-(defn- add-vec [& vecs]
-  (apply mapv + vecs))
+; TODO
+(defn- move-ball [{position :position :as state}]
+  state)
 
-(defn- reverse-x [[vx vy]] [(* -1 vx) vy])
-(defn- reverse-y [[vx vy]] [vx (* -1 vy)])
-
-(defn- hit-wall? [[x y]]
-  (or (< y 0) (> y screen-height)))
-
-(defn- hit-paddle? [[px py] [x y]]
-  (and (= px x) (>= y py) (<= y (+ py paddle-height))))
-
-(defn- apply-collisions [{position :position
-                          velocity :velocity
-                          paddle-left :paddle-left
-                          paddle-right :paddle-right}]
-  (cond
-    (hit-wall? position) (reverse-y velocity)
-    (hit-paddle? paddle-left position) (reverse-x velocity)
-    (hit-paddle? paddle-right position) (reverse-x velocity)
-    :else
-    velocity))
-
-(defn- update-state [{position :position :as state}]
-  (let [new-velocity (apply-collisions state)]
-    (assoc state
-           :velocity new-velocity
-           :position (add-vec position new-velocity))))
+; TODO
+(defn- move-paddles [{paddle-left :paddle-left paddle-right :paddle-right} c]
+  state)
 
 (defn- tick []
-  (swap! state update-state))
-
-(defn- move-paddle [paddle-up paddle-down [px py :as paddle] c]
-  (cond
-    (= c paddle-up) [px (- py paddle-move-distance)]
-    (= c paddle-down) [px (+ py paddle-move-distance)]
-    :else paddle))
-
-(def move-paddle-left (partial move-paddle paddle-left-up paddle-left-down))
-(def move-paddle-right (partial move-paddle paddle-right-up paddle-right-down))
-
-(defn- move-paddles [state c]
-  (-> state
-    (update :paddle-left move-paddle-left c)
-    (update :paddle-right move-paddle-right c)))
+  (swap! state move-ball))
 
 (defn- handle-keypress [e]
   (swap! state move-paddles (char (.-keyCode e))))
